d3933f83c3570bf00b5637c9ff5d2d1534593aa2
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2024-11-10 13:46:41 +0100

    first commit

---
+++ main/java/rentcar/rentcar/app/RentcarApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rentcar.rentcar.app;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class RentcarApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
